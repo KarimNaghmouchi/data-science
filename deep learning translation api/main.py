a13fbf6566ac183dfe648c41906f5c40a543ed47
KarimNaghmouchi <naghmouchi.karim2000@gmail.com>
2026-02-01 05:36:30 +0100

    translation API implementation with FastAPI and Pydantic

---
+++ deep learning translation api/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel, validator
+import tasks
+
+app = FastAPI()
+
+languages = ['English', 'French', 'German', 'Romanian']
+
+class Translation(BaseModel):
+    text: str
+    base_lang: str
+    final_lang: str
+
+    @validator('base_lang', 'final_lang')
+    def valid_lang(cls, lang):
+        if lang not in languages:
+            raise ValueError("Invalid language")
+        return lang
+
+## Route 1: /
+## Test if everything is working
+## {"message": "API is working!"}
+
+@app.get("/")
+def get_root():
+    return {"message": "API is working!"}
+
+
+## Route 2: /translate
+## Take in a translation request, and store it to the database
+## Return a translation id
+@app.post("/translate")
+def post_translation(t: Translation, background_tasks: BackgroundTasks):
+
+    # Store the translation
+    # Run the translation in the background
+    t_id = tasks.store_translation(t)
+    background_tasks.add_task(tasks.run_translation, t_id)
+    return {"task_id": t_id}
+## Route 3: /results
+## Take in a translation id
+## Return the translated text
+
+@app.get("/results")
+def get_translation(t_id: int):
+    return {"translation": tasks.find_translation(t_id)}
---
+++ deep learning translation api/models.py
@@ -0,0 +1,13 @@
+from peewee import Model, CharField, TextField, SqliteDatabase
+
+db = SqliteDatabase('translations.db')
+
+class TranslationModel(Model):
+    text = TextField()
+    base_lang = CharField()
+    final_lang = CharField()
+    translation = TextField(null=True)
+    class Meta:
+        database = db
+
+db.create_tables([TranslationModel])
